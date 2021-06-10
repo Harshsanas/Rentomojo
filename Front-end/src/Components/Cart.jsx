2df02a6e194ea2807266186d7302846ba53a5ecf
Harshsanas <harrysanas140@gmail.com>
2021-06-10 14:59:53 +0530

    resolving

---
+++ Front-end/src/Components/Cart.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const CART=styled.div`
+margin-top:60px;
+padding:50px 200px 50px 200px;`
+export default function Cart() {
+    return (
+      <CART>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+        <h1>THIS IS A CART PAGE </h1>
+      </CART>
+    );
+}
