b25c052e701850c87f5826a94c07e1884ab0b846
ajcodeskills <martinaj9514@gmail.com>
2021-06-10 16:17:01 +0530

    product header

---
+++ Front-end/src/components/header/ProductHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+export function ProductHeader() {
+    return (
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
