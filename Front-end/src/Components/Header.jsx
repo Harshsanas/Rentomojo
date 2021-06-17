5da79a1f021b5efb95b4825c1f14195af9261018
Harshsanas <harrysanas140@gmail.com>
2021-06-17 11:40:46 +0530

    resolve conflicts

--- Front-end/src/Components/Header.jsx
+++ Front-end/src/Components/Header.jsx
@@ -2,7 +2,6 @@
 import React, {  useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import rentomojo from "../image/rentomojo.png";
 import Modal from "react-modal";
 import cart from "../image/cart.jpg";
 import { Search } from './searchBar/Search';
@@ -93,21 +92,12 @@ const style = {
   },
 };
 
-// const INPUTSEC = styled.div`
-//   #mobile-num:hover {
-//     border-bottom: 2px solid black;
-//   }
-//   #mobile-num:focus {
-//     outline: none;
-//   }
-// `;
-
 export default function Header() {
 
   const [modalIsOpen,setModalIsOpen]=useState(false);
   const [acc, setAcc] = React.useState(false)
   const [isAuth, setIsAuth] = React.useState(JSON.parse(getUser('isAuth')));
-  const user = JSON.parse(getUser("user"));
+  const user = getUser("user");
   const [cityModal,setCityModal]=useState(false);
   const [cityName, setCityName] = React.useState("Pune");
   
@@ -132,7 +122,11 @@ export default function Header() {
         <HEADER>
           <div style={style.header}>
             <Link to="/">
-              <img src={rentomojo} alt="icon" width="150px" />
+              <img
+                src="https://www.rentomojo.com/public/images/logo.svg"
+                alt="icon"
+                width="150px"
+              />
             </Link>
             <button className="city-btn" onClick={() => setCityModal(true)}>
               {cityName}
@@ -160,48 +154,125 @@ export default function Header() {
                 </div>
               </button>
             </Link>
-            {!isAuth && <div>
-              <button id="login-btn" onClick={() => setModalIsOpen(true)}>
-                LOGIN / SIGNUP
-              </button>
-            </div>}
-            {isAuth && <div style = {{border:"1px solid green", height: 30,
-              width: 150, display:"flex", overflow: "hidden", padding: "5px 5px", borderRadius:"5px"
-            }} onClick = {()=>setAcc(!acc)}>
-                <div style = {{width: 30, height: 30, color: "#ffff", borderRadius:15, display:"flex",
-                  justifyContent:"center", alignItems:"center", background: "green", marginRight: "5px"
-              }}>{user.name[0]}</div>
-                <div style = {{display:"flex", alignItems:"center"}}>{user.name.split(" ")[0]}</div>
-              </div>}
+            {!isAuth && (
+              <div>
+                <button id="login-btn" onClick={() => setModalIsOpen(true)}>
+                  LOGIN / SIGNUP
+                </button>
+              </div>
+            )}
+            {isAuth && (
+              <div
+                style={{
+                  border: "1px solid green",
+                  height: 30,
+                  width: 150,
+                  display: "flex",
+                  overflow: "hidden",
+                  padding: "5px 5px",
+                  borderRadius: "5px",
+                }}
+                onClick={() => setAcc(!acc)}
+              >
+                <div
+                  style={{
+                    width: 30,
+                    height: 30,
+                    color: "#ffff",
+                    borderRadius: 15,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    background: "green",
+                    marginRight: "5px",
+                  }}
+                >
+                  {user.name[0]}
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  {user.name.split(" ")[0]}
+                </div>
+              </div>
+            )}
           </div>
-          {acc && <div style = {{display: "flex", flexDirection:"column",
-                    width: 150,
-                    position:"absolute",
-                    top:50,
-                    left: "77%",
-                    padding: "10px 10px 10px 10px",
-                    background: "#ffff",
-                    border: "1px solid grey",
-                    borderRadius:"5px"
-
-          }}>
-              <div style = {{display: "flex", cursor: "pointer", alignItems:"center", margin:"10px 0"}}>
-                <img src="https://static.thenounproject.com/png/3542378-200.png" style = {{width:20, marginRight:"10px"}} alt=""/>
-                Account</div>
-              <div style = {{display: "flex", cursor: "pointer", alignItems:"center", margin:"10px 0"}}>
-                <img src="https://pics.freeicons.io/uploads/icons/png/17773426191535958157-512.png" style = {{width:20, marginRight:"10px"}} alt=""/>
-                Settings</div>
-              <div style = {{display: "flex", cursor: "pointer", alignItems:"center", margin:"10px 0"}}>
-                  <img src="https://image.shutterstock.com/image-vector/heart-plus-pozitive-wishlist-icon-260nw-666321889.jpg" style = {{width:20, marginRight:"10px"}} alt=""/>
-                My Wishlist</div>
-              <div style = {{cursor: "pointer",
-              display: "flex",alignItems:"center", margin:"10px 0"
-
-            }} onClick = {handleLogOut}>
-              <img src="https://cdn1.iconfinder.com/data/icons/ios-edge-line-6/25/Logout_1-512.png" style = {{width:20, marginRight:"10px"}} alt=""/>
-              
-              Logout</div>
-            </div>}
+          {acc && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: 150,
+                position: "absolute",
+                top: 50,
+                left: "77%",
+                padding: "10px 10px 10px 10px",
+                background: "#ffff",
+                border: "1px solid grey",
+                borderRadius: "5px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  cursor: "pointer",
+                  alignItems: "center",
+                  margin: "10px 0",
+                }}
+              >
+                <img
+                  src="https://static.thenounproject.com/png/3542378-200.png"
+                  style={{ width: 20, marginRight: "10px" }}
+                  alt=""
+                />
+                Account
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  cursor: "pointer",
+                  alignItems: "center",
+                  margin: "10px 0",
+                }}
+              >
+                <img
+                  src="https://pics.freeicons.io/uploads/icons/png/17773426191535958157-512.png"
+                  style={{ width: 20, marginRight: "10px" }}
+                  alt=""
+                />
+                Settings
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  cursor: "pointer",
+                  alignItems: "center",
+                  margin: "10px 0",
+                }}
+              >
+                <img
+                  src="https://image.shutterstock.com/image-vector/heart-plus-pozitive-wishlist-icon-260nw-666321889.jpg"
+                  style={{ width: 20, marginRight: "10px" }}
+                  alt=""
+                />
+                My Wishlist
+              </div>
+              <div
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                  margin: "10px 0",
+                }}
+                onClick={handleLogOut}
+              >
+                <img
+                  src="https://cdn1.iconfinder.com/data/icons/ios-edge-line-6/25/Logout_1-512.png"
+                  style={{ width: 20, marginRight: "10px" }}
+                  alt=""
+                />
+                Logout
+              </div>
+            </div>
+          )}
         </HEADER>
         {modalIsOpen && (
           <div style={style.signInModal}>
@@ -211,7 +282,7 @@ export default function Header() {
 
         {cityModal && (
           <div style={style.cityModal}>
-          <Allcity handleClose={handleClose} handleCity={handleCity}/>
+            <Allcity handleClose={handleClose} handleCity={handleCity} />
           </div>
         )}
       </>
